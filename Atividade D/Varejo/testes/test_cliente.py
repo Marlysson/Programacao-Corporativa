38f2bd45b52174ce9ce31ebad355b5e81e17cc34
Marlysson Silva <marlysson5@gmail.com>
2016-06-21 20:44:36 -0300

    Adicionando teste para a classe Cliente.

--- Atividade D/Varejo/testes/test_cliente.py
+++ Atividade D/Varejo/testes/test_cliente.py
@@ -3,7 +3,7 @@
 import unittest
 import sys,os
 
-#Adicionando pasta externa para capturar os modelos
+# Adicionando pasta externa para capturar os modelos
 pasta_projeto = os.path.abspath("../")
 sys.path.append(pasta_projeto)
 
@@ -11,10 +11,25 @@ from modelo.cliente import Cliente
 
 class TestaCliente(unittest.TestCase):
 
-	def test_cliente(self):
-		cliente = Cliente("Marlysson")
+	def test_nome_cliente(self):
 
-		self.assertEqual(cliente.nome,"Marlysson")
+		self.cliente = Cliente("Marlysson")
+
+		self.assertEqual(self.cliente.nome,"Marlysson")
+
+	def test_pontos_cliente(self):
+		self.cliente = Cliente("Marlysson")
+		self.cliente.pontos = 10
+
+		self.assertEqual(self.cliente.pontos,10)
+
+	def test_soma_pontos_cliente(self):
+		self.cliente = Cliente("Marcelo")
+
+		self.cliente.pontos = 5
+		self.cliente.pontos += 10
+
+		self.assertEqual(self.cliente.pontos,15)
 
 if __name__ == "__main__":
 	unittest.main()
