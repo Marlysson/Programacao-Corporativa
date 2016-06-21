3cfdb54cb3b86b01356b18e7312195a48ce429c0
Marlysson Silva <marlysson5@gmail.com>
2016-06-21 20:54:01 -0300

    Preparando arquivo para receber a composição de outras entidades.

--- Atividade D/Varejo/modelo/compra.py
+++ Atividade D/Varejo/modelo/compra.py
@@ -1,6 +1,20 @@
 # -*- coding: utf8 -*-
 
+from cliente import Cliente
+from carrinho import Carrinho
+from produto import Produto
+
 class Compra(object):
-	def __init__(self,nome):
-		self.cliente = Cliente(nome)
-		
+
+	def __init__(self,cliente):
+		self._cliente = cliente
+		self._carrinho = Carrinho()
+		self._descontos = list()
+
+	@property
+	def cliente(self):
+		return self._cliente
+
+	@property
+	def carrinho(self):
+		return self._carrinho
